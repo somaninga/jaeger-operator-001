7a2901c20d39bd3107055a3ef1106b4fb743f50f
somaninga_pujari <somaninga@paramountsoft.net>
2022-06-07 13:32:16 +0000

    Cleaned up the code

--- service-b/formatter.js
+++ service-b/formatter.js
@@ -1,15 +1,11 @@
-const opentracing = require('opentracing')
-const tracer = opentracing.globalTracer()
+
 
 function formatGreeting(req, res) {
-  const span = tracer.startSpan('format-greeting', { childOf: req.span })
   // Parse the handler input
   const name = req.query.name
-  span.log({ event: 'format', message: `formatting message remotely for name ${name}` })
+
   const response = `Hello from service-b ${name}!`
-  const baggage = span.getBaggageItem('my-baggage')
-  span.log({ event: 'baggage', message: `this is baggage ${baggage}` })
-  span.finish()
+
   res.send(response)
 }
 module.exports = formatGreeting
